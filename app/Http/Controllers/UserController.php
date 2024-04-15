2c3d4d63ad7dba3de394f317cc7c7156285eb949
Orchid <orchid.b27@gmail.com>
2024-04-15 15:42:43 +0600

    v1

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+
+class UserController extends Controller
+{
+    public function uploadImage(mixed $file): string
+    {
+        $imageName = uniqid() . '.' . $file->getClientOriginalExtension();
+        $file->move('uploads/user/', $imageName);
+        return $imageName;
+    }
+
+    public function index()
+    {
+        return view('dashboard.user.index');
+    }
+
+    public function create(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'firstname'      => 'required',
+            'email'     => 'required|email|unique:users,email',
+            'image'     => 'image',
+            'password'  => 'required|min:5|max:30',
+            'cpassword' => 'required|min:5|max:30|same:password'
+        ]);
+
+        $imageName      = $request->hasFile('image') ? $this->uploadImage($request->file('image'))
+            : null;
+
+        $user = new User();
+        $user->firstname     = $request->input('firstname');
+        $user->lastname      = $request->input('lastname') ?? null;
+        $user->email         = $request->input('email');
+        $user->password      = ($request->password);
+        $user->image         = $imageName ?? null;
+        $user->review        = $request->input('review') ?? null;
+        $user->save();
+
+        Auth::guard('web')->login($user);
+
+        return redirect()->route('user.index')->with('success', 'You are now registered successfully');
+    }
+
+    public function check(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email'    => 'required|email|exists:users,email',
+            'password' => 'required|min:5|max:30'
+        ], [
+            'email.exists' => 'This email is not exists on user table'
+        ]);
+
+        return   Auth::guard('web')->attempt($request->only('email', 'password'))
+            ? redirect()->route('user.index')->with('success', 'you are logged in')
+            : redirect()->route('user.login')->with('fail', 'Incorrect credentials');
+    }
+}
+
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\userController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::prefix('user')->name('user.')->group(function(){
+
+    Route::middleware('guest')->group(function(){
+
+        Route::view('/login', 'dashboard.user.login')->name('login');
+        Route::view('/register', 'dashboard.user.register')->name('register');
+        Route::post('/create', [UserController::class, 'create'])->name('create');
+        Route::post('/user/login', [UserController::class, 'check'])->name('check');
+    });
+
+    Route::middleware('auth')->group(function(){
+
+        Route::get('/home', [UserController::class, 'index'])->name('index');
+    });
+
+});
